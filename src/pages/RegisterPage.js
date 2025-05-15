43e760f2da6fca56a8f0c24eef09f390f7afb77c
yrnsr <213301066@ogr.selcuk.edu.tr>
2025-05-15 16:30:34 +0300

    güncellendii

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './RegisterPage.css';
 
+
 const RegisterPage = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
